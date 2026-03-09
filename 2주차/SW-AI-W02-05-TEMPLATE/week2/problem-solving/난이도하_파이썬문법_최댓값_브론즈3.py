a3459dd2ac5ace7f3bf5345ecd9ebb752fc9802e
최현진 <guswls1478@gmail.com>
2026-03-09 11:55:35 +0900

    백준 하 최댓값 제출

--- 2주차/SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ 2주차/SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,20 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+a = []
+max =0
+count = 0
+for i in range (9):
+    b = int(input())
+    a.append(b)
+
+max = a[0]
+for j in range (9):
+    if max < a[j]:
+        max = a[j]
+        count =j
+
+
+print(max)
+print(count+1)
+
+
