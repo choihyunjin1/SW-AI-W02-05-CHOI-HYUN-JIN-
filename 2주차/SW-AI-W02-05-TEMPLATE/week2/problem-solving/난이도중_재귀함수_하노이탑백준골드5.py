dbcdc5b9d12fb795f089517f108202c81086a5e3
최현진 <guswls1478@gmail.com>
2026-03-10 00:22:18 +0900

    문자열, 하노이 완성

--- 2주차/SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ 2주차/SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,16 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+def dohanoi(n, start, end, tmp):
+    if n == 1:
+        print(start, end)
+        return
+    
+    dohanoi(n - 1, start, tmp, end)
+    print(start, end)
+    dohanoi(n - 1, tmp, end, start)
+
+n = int(input())
+
+print(2 ** n - 1)
+if n <= 20:
+    dohanoi(n, 1, 3, 2)
