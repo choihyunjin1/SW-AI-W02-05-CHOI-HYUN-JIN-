3ff45cd3dfbfa7897cd56207ed7c86d2761aa1c8
최현진 <guswls1478@gmail.com>
2026-03-09 16:45:56 +0900

    난이도 하 완료

--- 2주차/SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ 2주차/SW-AI-W02-05-TEMPLATE/week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,27 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+RAW = input()
+result =""
+duply = RAW.upper()
+
+count = {}
+for ch in duply:
+    if ch in count:
+        count[ch] += 1
+    else:
+        count[ch] = 1
+
+maxcnt = 0
+
+
+max_value = max(count.values())
+for key, value in count.items():
+    if value == max_value:
+        maxcnt += 1
+        result = key
+
+if maxcnt > 1:
+    print("?")
+else:    
+    print(result)
