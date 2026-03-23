1f4a1b99ba15944ece2495b24748b18d848d58b3
최현진 <guswls1478@gmail.com>
2026-03-23 11:24:59 +0900

    feat: 문제 해결을 위한 위상 정렬 및 이진 검색 트리 구현 추가 chore: 불필요한 테스트 파일 삭제

--- 2~5주차/SW-AI-W02-05-TEMPLATE/week4/problem-solving/난이도중_위상정렬_작업_골드4.py
+++ 2~5주차/SW-AI-W02-05-TEMPLATE/week4/problem-solving/난이도중_위상정렬_작업_골드4.py
@@ -1,2 +1,46 @@
 # 위상정렬 - 작업 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2056
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+work_time = [0] * (n + 1)
+finish_time = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    
+    work_time[i] = data[0]
+    cnt = data[1]
+    
+    for j in range(2, 2 + cnt):
+        prev = data[j]
+        graph[prev].append(i)
+        indegree[i] += 1
+
+queue = deque()
+
+for i in range(1, n + 1):
+    if indegree[i] == 0:
+        queue.append(i)
+        finish_time[i] = work_time[i]
+
+while queue:
+    now = queue.popleft()
+
+    for next_node in graph[now]:
+        if finish_time[next_node] < finish_time[now] + work_time[next_node]:
+            finish_time[next_node] = finish_time[now] + work_time[next_node]
+
+        indegree[next_node] -= 1
+
+        if indegree[next_node] == 0:
+            queue.append(next_node)
+
+print(max(finish_time))
--- 2~5주차/SW-AI-W02-05-TEMPLATE/week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ 2~5주차/SW-AI-W02-05-TEMPLATE/week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,40 @@
 # 트리 - 이진 검색 트리 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/5639
+
+
+import sys
+sys.setrecursionlimit(10**6)
+
+tree = []
+
+while True:
+    line = sys.stdin.readline()
+    
+    if not line:  # 입력 끝
+        break
+
+    if line.strip() == "":
+        break
+
+    tree.append(int(line))
+
+
+def postorder(tree):
+    if not tree:
+        return
+    
+    root = tree[0]
+    idx = len(tree)
+    for i in range(1, len(tree)):
+            if root < tree[i]:
+                idx = i
+                break
+    left = tree[1:idx]
+    right = tree[idx:]
+
+    postorder(left)
+    postorder(right)
+    print(root)
+
+
+postorder(tree)
