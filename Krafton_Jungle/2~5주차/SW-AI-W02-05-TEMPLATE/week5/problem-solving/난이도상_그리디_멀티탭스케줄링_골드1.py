222a1cf229ef5de233a4cf9472e232d3219941d1
최현진 <guswls1478@gmail.com>
2026-03-28 17:01:55 +0900

    ㅇㅇㅇㅇ

--- Krafton_Jungle/2~5주차/SW-AI-W02-05-TEMPLATE/week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
+++ Krafton_Jungle/2~5주차/SW-AI-W02-05-TEMPLATE/week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
@@ -1,2 +1,34 @@
 # 그리디 - 멀티탭 스케줄링 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1700
+n, k = map(int, input().split())
+
+multi = list(map(int, input().split()))
+cnt = 0
+
+result = []
+
+for i in range(k):
+    if multi[i] in result:
+        continue
+
+    if len(result) < n:
+        result.append(multi[i])
+        continue
+
+    idx = -1
+    farthest = -1
+
+    for j in range(n):
+        if result[j] not in multi[i+1:]:
+            idx = j
+            break
+        else:
+            next_use = multi[i+1:].index(result[j])
+            if next_use > farthest:
+                farthest = next_use
+                idx = j
+
+    result[idx] = multi[i]
+    cnt += 1
+
+print(cnt)
--- Krafton_Jungle/2~5주차/SW-AI-W02-05-TEMPLATE/week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ Krafton_Jungle/2~5주차/SW-AI-W02-05-TEMPLATE/week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,25 +1,78 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
 
+import sys
+input = sys.stdin.readline
+
 T = int(input())
+result = []
 
-score = []
-
-for _ in range (T):
+for _ in range(T):
     N = int(input())
-    score = []
 
+    score_list = []
     for i in range(N):
-        score.append(list(map(int,input().split())))
+        score_list.append(tuple(map(int, input().split())))
 
-    score.sort(key = lambda x: x[0])
+    paper_sorted = sorted(score_list, key=lambda x: x[0])
 
-    count =1
-    min_score = score[0][1]
+    interview_sorted = sorted(score_list, key=lambda x: x[1])
 
-    for i in range(1,N):
-        if score[i][1] < min_score:
-            count+=1
-            min_score = score[i][1]
-    print(count)
+    interview_rank = {}
+    for i in range(N):
+        interview_rank[interview_sorted[i]] = i                        
+                                                                                            #     interview_rank = {
+                                                                                            #     (4,1): 0,
+                                                                                            #     (3,2): 1,
+                                                                                            #     (2,3): 2,
+                                                                                            #     (1,4): 3 }
+                                                                                            # 
+                                                                                            #  score_list
+                                                                                            #    
+                                                                                            # paper_sorted  (서류 기준 정렬)
+                                                                                            # interview_sorted ->  interview_rank (면접 순위표)
+                                                                                            #    
+                                                                                            # for score in paper_sorted:
+                                                                                            #     interview_rank[score] vs min_rank 비교
+ 
+    count = 0
+    min_rank = N 
+    # [ (1,4), (2,3), (3,2), (4,1) ]
+    #      4      3     2      1
+    # [ (2,3), (3,2), (4,1) ]
+    for score in paper_sorted:
+        if interview_rank[score] < min_rank:
+            count += 1
+            min_rank = interview_rank[score]
+
+    result.append(count)
+
+for i in result:
+    print(i)
+
+
+# import sys
+# input = sys.stdin.readline
+
+# T = int(input())
+
+# for _ in range(T):
+#     N = int(input())
+#     score = []
+
+#     for i in range(N):
+#         score.append(list(map(int, input().split())))
+
+#     score.sort(key=lambda x: x[1])
+#     score.sort(key=lambda x: x[0])
+
+#     count = 1
+#     min_score = score[0][1]
+
+#     for i in range(1, N):
+#         if score[i][1] < min_score:
+#             count += 1
+#             min_score = score[i][1]
+
+#     print(count)
 
